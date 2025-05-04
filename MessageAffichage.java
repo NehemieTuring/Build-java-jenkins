27021c4724ef4c410e68aa9ad1c870a08998d528
Nehemie Turing <ewanenehemie30@gmail.com>
2025-05-04 05:15:28 +0100

    comment compiler dans maven ou graddle

---
+++ MessageAffichage.java
@@ -0,0 +1,7 @@
+public class MessageAffichage {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, comment vous allez ??");
+        System.out.println("Dans cette partie nous allons vous montrer comment compiler votre code Java avec Jenkins.");
+        System.out.println("Aurevoir tout le monde et merci pour votre attention.");
+    }
+}
